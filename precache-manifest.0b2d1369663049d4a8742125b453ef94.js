65b1499d1dc805dcde3b941c12a04ae3ca91b049
jcoriell <47751211+jcoriell@users.noreply.github.com>
2019-11-11 18:12:57 -0600

    Updates

---
+++ precache-manifest.0b2d1369663049d4a8742125b453ef94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c520b45f51ce58ec6fe937299c1ba311",
+    "url": "/othello_deployed/index.html"
+  },
+  {
+    "revision": "c7e2724df87264875f80",
+    "url": "/othello_deployed/static/css/main.a1ff9502.chunk.css"
+  },
+  {
+    "revision": "9d7c3e6c87c810811b22",
+    "url": "/othello_deployed/static/js/2.d16d2d18.chunk.js"
+  },
+  {
+    "revision": "c7e2724df87264875f80",
+    "url": "/othello_deployed/static/js/main.fcc1477c.chunk.js"
+  },
+  {
+    "revision": "8c1d61e80e0da049bd30",
+    "url": "/othello_deployed/static/js/runtime-main.501927c8.js"
+  }
+]);
